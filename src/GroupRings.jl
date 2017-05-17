cdc2326e06fce661c940cccb9d175535b4de9ac0
kalmar <kalmar@amu.edu.pl>
2017-05-17 12:28:22 +0200

    import getindex, setindex!

--- src/GroupRings.jl
+++ src/GroupRings.jl
@@ -3,7 +3,7 @@ module GroupRings
 using Nemo
 import Nemo: Group, GroupElem, Ring, parent, elem_type, parent_type
 
-import Base: convert, show, hash, ==, +, -, *, //, /, length, norm, rationalize, deepcopy_internal
+import Base: convert, show, hash, ==, +, -, *, //, /, length, norm, rationalize, deepcopy_internal, getindex, setindex!
 
 ###############################################################################
 #
