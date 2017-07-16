c1e2d4209adb1319d286bc01eb945056af1745b7
kalmar <kalmar@amu.edu.pl>
2017-07-16 21:45:24 +0200

    to match e.g. sparse and dense zeros... ugly

--- src/GroupRings.jl
+++ src/GroupRings.jl
@@ -167,7 +167,7 @@ function deepcopy_internal(X::GroupRingElem, dict::ObjectIdDict)
 end
 
 function hash(X::GroupRingElem, h::UInt)
-   return hash(X.coeffs, hash(parent(X), h))
+   return hash(full(X.coeffs), hash(parent(X), hash(GroupRingElem, h)))
 end
 
 function getindex(X::GroupRingElem, n::Int)
